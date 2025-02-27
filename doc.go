94c964b605e086968a4f6c985ae6e78da54bac91
Carl Kittelberger <icedream@icedream.pw>
2025-02-27 21:08:02 +0100

    Add package doc.

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+ * Package bsdiff implements very high-level functionality to patch and diff two
+ * contents.
+ *
+ * This package relies on the bsdiff C code and hence requires cgo to be
+ * enabled.
+ */
+package bsdiff
