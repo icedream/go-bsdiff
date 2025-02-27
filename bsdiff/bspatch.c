drake127/bsdiff

Fixes CVE-2014-9862 (https://www.x41-dsec.de/lab/advisories/x41-2020-006-bspatch/).

bspatch.c
@@ -62,7 +62,9 @@ int bspatch(const uint8_t* source, int64_t sourcesize, uint8_t* target, int64_t 
 		};
 
 		/* Sanity-check */
-		if(newpos+ctrl[0]>targetsize)
+		if (ctrl[0]<0 || ctrl[0]>INT_MAX ||
+			ctrl[1]<0 || ctrl[1]>INT_MAX ||
+			newpos+ctrl[0]>targetsize)
 			return -1;
 
 		/* Read diff string */

icedream/go-bsdiff

bsdiff/bspatch.c
@@ -62,7 +62,9 @@ int bspatch(const uint8_t* old, int64_t oldsize, uint8_t* new, int64_t newsize, 
 		};
 
 		/* Sanity-check */
-		if(newpos+ctrl[0]>newsize)
+		if (ctrl[0]<0 || ctrl[0]>INT_MAX ||
+			ctrl[1]<0 || ctrl[1]>INT_MAX ||
+			newpos+ctrl[0]>newsize)
 			return -1;
 
 		/* Read diff string */